eba0e3d72d687c40d4b979849fe966ac5b93dda0
Shivanshi <1999shivanshi.patel@gmail.com>
2021-01-11 17:38:12 +0530

    Updates

---
+++ precache-manifest.35372deeec0a8580e4cf9dc50ff74ab0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1074a96e749109e0f7d82b12314115",
+    "url": "/cricket/index.html"
+  },
+  {
+    "revision": "b7100b9205059985c022",
+    "url": "/cricket/static/css/main.a4049767.chunk.css"
+  },
+  {
+    "revision": "1b6c28166b0c32c76c4f",
+    "url": "/cricket/static/js/2.06e84c12.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cricket/static/js/2.06e84c12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7100b9205059985c022",
+    "url": "/cricket/static/js/main.5d84bc70.chunk.js"
+  },
+  {
+    "revision": "f7af70d9017449f104aa",
+    "url": "/cricket/static/js/runtime-main.a8939453.js"
+  }
+]);
